983a38a44c748d84fb4667cc08444372143f6f45
FX - PR0CESS <fx.e.morin@gmail.com>
2022-06-12 13:01:50 -0400

    Add Conditional Mixin

---
+++ src/main/java/ca/fxco/moreculling/utils/MixinConfigPlugin.java
@@ -0,0 +1,21 @@
+package ca.fxco.moreculling.utils;
+
+import me.fallenbreath.conditionalmixin.api.mixin.RestrictiveMixinConfigPlugin;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinConfigPlugin extends RestrictiveMixinConfigPlugin {
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+}
