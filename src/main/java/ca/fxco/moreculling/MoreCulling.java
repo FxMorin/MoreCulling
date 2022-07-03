d26c57705f83620510283a92ce97a17fdea7385c
FX - PR0CESS <fx.e.morin@gmail.com>
2022-07-02 22:04:03 -0400

    Add Logger

--- src/main/java/ca/fxco/moreculling/MoreCulling.java
+++ src/main/java/ca/fxco/moreculling/MoreCulling.java
@@ -6,6 +6,8 @@ import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MoreCulling implements ClientModInitializer {
 
@@ -13,6 +15,8 @@ public class MoreCulling implements ClientModInitializer {
 
     public static final String MOD_ID = "moreculling";
     public static final TagKey<Block> DONT_CULL = TagKey.of(Registry.BLOCK_KEY, new Identifier(MOD_ID, "dont_cull"));
+
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitializeClient() {}
