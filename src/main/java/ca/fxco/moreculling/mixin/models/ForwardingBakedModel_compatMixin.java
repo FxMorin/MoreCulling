73fc5dd978ae617416d4af4c53d7c76f41b1d10b
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-18 11:16:39 -0400

    Fix Fabric-API compat This fixes many mod compatibility issues, including lambdaBetterGrass

---
+++ src/main/java/ca/fxco/moreculling/mixin/models/ForwardingBakedModel_compatMixin.java
@@ -0,0 +1,22 @@
+package ca.fxco.moreculling.mixin.models;
+
+import ca.fxco.moreculling.api.model.BakedOpacity;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel;
+import net.minecraft.client.render.model.BakedModel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Restriction(require = @Condition("fabric-renderer-api-v1"))
+@Mixin(ForwardingBakedModel.class)
+public class ForwardingBakedModel_compatMixin implements BakedOpacity {
+
+    @Shadow
+    protected BakedModel wrapped;
+
+    @Override
+    public boolean hasTextureTranslucency() {
+        return !(this.wrapped instanceof BakedOpacity bakedOpacity && !bakedOpacity.hasTextureTranslucency());
+    }
+}
