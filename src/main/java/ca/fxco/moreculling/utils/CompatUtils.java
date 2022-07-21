c8b7e28c80b918ce6dfe47c3e62eb9032bc7a474
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-21 19:00:55 -0400

    Use fabric loader while quilt still supports it

--- src/main/java/ca/fxco/moreculling/utils/CompatUtils.java
+++ src/main/java/ca/fxco/moreculling/utils/CompatUtils.java
@@ -1,6 +1,8 @@
 package ca.fxco.moreculling.utils;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 public class CompatUtils {
-    public static final boolean IS_SODIUM_LOADED = LoaderUtils.isModLoaded("sodium");
-    public static final boolean IS_CULLLESSLEAVES_LOADED = LoaderUtils.isModLoaded("cull-less-leaves");
+    public static final boolean IS_SODIUM_LOADED = FabricLoader.getInstance().isModLoaded("sodium");
+    public static final boolean IS_CULLLESSLEAVES_LOADED = FabricLoader.getInstance().isModLoaded("cull-less-leaves");
 }
