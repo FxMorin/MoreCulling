f5418a917f67061098798146602dbdae2b41b516
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-26 15:27:28 -0400

    Use new opacity settings. This allows us to gain a huge amount of performance, making it possible for a lot more blocks to cull.

--- src/main/java/ca/fxco/moreculling/utils/SpriteUtils.java
+++ src/main/java/ca/fxco/moreculling/utils/SpriteUtils.java
@@ -2,6 +2,7 @@ package ca.fxco.moreculling.utils;
 
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.texture.Sprite;
+import org.lwjgl.system.MemoryUtil;
 
 public class SpriteUtils {
 
@@ -21,7 +22,7 @@ public class SpriteUtils {
             int width = nativeImage.getWidth();
             for (int y = 0; y < nativeImage.getHeight(); ++y)
                 for (int x = 0; x < width; ++x)
-                    if (nativeImage.getOpacity(x, y) == 1)
+                    if (SpriteUtils.getOpacity(nativeImage, x, y) == 0)
                         return true;
         }
         return false;
@@ -32,9 +33,28 @@ public class SpriteUtils {
             int width = nativeImage.getWidth();
             for (int y = 0; y < nativeImage.getHeight(); ++y)
                 for (int x = 0; x < width; ++x)
-                    if (nativeImage.getOpacity(x, y) != 0)
+                    if (SpriteUtils.getOpacity(nativeImage, x, y) != -1)
                         return true;
         }
         return false;
+    }
+
+    // Minecraft's Luminance-Alpha format was screwing some results. We look at alpha directly here instead
+    public static byte getOpacity(NativeImage img, int x, int y) {
+        if (!img.getFormat().hasAlpha()) return 0;
+        int i = (x + y * img.getWidth()) * img.getFormat().getChannelCount() + img.getFormat().getAlphaOffset() / 8;
+        return MemoryUtil.memGetByte(img.pointer + (long)i);
+    }
+
+    public static void printOpacity(NativeImage nativeImage) {
+        if (nativeImage.getFormat().hasOpacityChannel()) {
+            int width = nativeImage.getWidth();
+            for (int y = 0; y < nativeImage.getHeight(); ++y) {
+                StringBuilder line = new StringBuilder();
+                for (int x = 0; x < width; ++x)
+                    line.append(String.format("%4d"+(x != width-1 ? "," : ""),SpriteUtils.getOpacity(nativeImage, x, y)));
+                System.out.println(line);
+            }
+        }
     }
 }
