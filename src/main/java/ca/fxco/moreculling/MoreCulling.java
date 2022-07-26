0bc5461a90674009e480065d2edc41f8e58945ec
FX - PR0CESS <28154542+fmorin@users.noreply.github.com>
2022-07-26 15:25:30 -0400

    no longer being used

--- src/main/java/ca/fxco/moreculling/MoreCulling.java
+++ src/main/java/ca/fxco/moreculling/MoreCulling.java
@@ -12,8 +12,6 @@ import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class MoreCulling implements ClientModInitializer {
 
     public static BlockRenderManager blockRenderManager = null;
