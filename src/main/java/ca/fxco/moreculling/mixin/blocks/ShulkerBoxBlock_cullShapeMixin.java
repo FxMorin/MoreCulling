7ce29e904241cf2d94114230e739850bd10a1ab5
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-01-03 05:40:27 -0500

    Remove shulker culling
    (cherry picked from commit c4daa741faadcede39f34c8fb3be88a835599d95)

--- src/main/java/ca/fxco/moreculling/mixin/blocks/ShulkerBoxBlock_cullShapeMixin.java
+++ src/main/java/ca/fxco/moreculling/mixin/blocks/ShulkerBoxBlock_cullShapeMixin.java
@@ -1,12 +1,7 @@
 package ca.fxco.moreculling.mixin.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ShulkerBoxBlock.class)
@@ -17,10 +12,11 @@ public class ShulkerBoxBlock_cullShapeMixin extends Block {
     }
 
 
-    @Override
+    // Should only be done in LOD Chunks
+    /*@Override
     public VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
         VoxelShape voxelShape = state.getOutlineShape(world, pos);
         if (!voxelShape.equals(VoxelShapes.fullCube())) return VoxelShapes.empty();
         return voxelShape;
-    }
+    }*/
 }
