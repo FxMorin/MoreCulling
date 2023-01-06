adfc3a7d15ab4de99e6a9f3d5e7341335ba168b7
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-01-06 17:40:36 -0500

    Add bottom culling for ShulkerBox

--- src/main/java/ca/fxco/moreculling/mixin/blocks/ShulkerBoxBlock_cullShapeMixin.java
+++ src/main/java/ca/fxco/moreculling/mixin/blocks/ShulkerBoxBlock_cullShapeMixin.java
@@ -1,22 +1,24 @@
 package ca.fxco.moreculling.mixin.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlock_cullShapeMixin extends Block {
 
+    private static final VoxelShape CULL_BOTTOM = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
+
     public ShulkerBoxBlock_cullShapeMixin(Settings settings) {
         super(settings);
     }
 
-
-    // Should only be done in LOD Chunks
-    /*@Override
+    @Override
     public VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
-        VoxelShape voxelShape = state.getOutlineShape(world, pos);
-        if (!voxelShape.equals(VoxelShapes.fullCube())) return VoxelShapes.empty();
-        return voxelShape;
-    }*/
+        return CULL_BOTTOM;
+    }
 }
