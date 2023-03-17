02f1629352b6d60efd1152e35b9c9ba3611bed4e
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-03-17 06:12:25 -0400

    Prevent Rails and Ladders from culling. Fix Leaves and Doors Fixes #143
    (cherry picked from commit beb866a464c8950822d285eb0ee0c5b0c95250e2)

--- src/main/java/ca/fxco/moreculling/mixin/blocks/LadderBlock_cullAgainstMixin.java
+++ src/main/java/ca/fxco/moreculling/mixin/blocks/LadderBlock_cullAgainstMixin.java
@@ -2,12 +2,12 @@ package ca.fxco.moreculling.mixin.blocks;
 
 import ca.fxco.moreculling.api.block.MoreBlockCulling;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.DoorBlock;
+import net.minecraft.block.LadderBlock;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(DoorBlock.class)
-public class DoorBlock_cullAgainstMixin implements MoreBlockCulling {
+@Mixin(LadderBlock.class)
+public class LadderBlock_cullAgainstMixin implements MoreBlockCulling {
 
     @Override
     public boolean cantCullAgainst(BlockState state, Direction side) {
