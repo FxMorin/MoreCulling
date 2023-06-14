e8776bd085f2b3d91451da536f9126d85312eec8
fxmorin <28154542+fxmorin@users.noreply.github.com>
2023-06-14 08:22:45 -0400

    Fix Sign Culling, it now culls both sides making it 2x more effective. Thanks Mojang...

--- src/main/java/ca/fxco/moreculling/mixin/renderers/SignBlockEntityRenderer_textMixin.java
+++ src/main/java/ca/fxco/moreculling/mixin/renderers/SignBlockEntityRenderer_textMixin.java
@@ -2,22 +2,20 @@ package ca.fxco.moreculling.mixin.renderers;
 
 import ca.fxco.moreculling.MoreCulling;
 import ca.fxco.moreculling.utils.MathUtils;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SignBlock;
-import net.minecraft.block.WallSignBlock;
-import net.minecraft.block.WoodType;
-import net.minecraft.block.entity.SignBlockEntity;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.SignText;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import static ca.fxco.moreculling.utils.CullingUtils.shouldHideWallSignText;
 import static ca.fxco.moreculling.utils.MathUtils.ONE_SIGN_ROTATION;
@@ -25,46 +23,66 @@ import static ca.fxco.moreculling.utils.MathUtils.ONE_SIGN_ROTATION;
 @Mixin(SignBlockEntityRenderer.class)
 public class SignBlockEntityRenderer_textMixin {
 
-    @Inject(
+    @WrapWithCondition(
             method = "render(Lnet/minecraft/block/entity/SignBlockEntity;" +
-                    "FLnet/minecraft/client/util/math/MatrixStack;" +
-                    "Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
-            locals = LocalCapture.CAPTURE_FAILSOFT,
+                    "Lnet/minecraft/client/util/math/MatrixStack;" +
+                    "Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/block/BlockState;" +
+                    "Lnet/minecraft/block/AbstractSignBlock;" +
+                    "Lnet/minecraft/block/WoodType;Lnet/minecraft/client/model/Model;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/block/entity/SignBlockEntityRenderer;renderText(" +
-                            "Lnet/minecraft/block/entity/SignBlockEntity;" +
+                    target = "Lnet/minecraft/client/render/block/entity/SignBlockEntityRenderer;" +
+                            "renderText(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/SignText;" +
                             "Lnet/minecraft/client/util/math/MatrixStack;" +
-                            "Lnet/minecraft/client/render/VertexConsumerProvider;IF)V"
-            ),
-            cancellable = true
+                            "Lnet/minecraft/client/render/VertexConsumerProvider;IIIZ)V",
+                    ordinal = 0
+            )
     )
-    private void cullSignText(SignBlockEntity signBlockEntity, float tickDelta, MatrixStack matrixStack,
-                              VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo ci,
-                              BlockState blockState, float g, WoodType signType,
-                              SignBlockEntityRenderer.SignModel signModel) {
+    private boolean cullFrontSignText(SignBlockEntityRenderer renderer, BlockPos pos, SignText text,
+                                      MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i,
+                                      int j, int i2, boolean l, @Local BlockState state, @Local Model model) {
+        return cullSignText(pos, state, model, true);
+    }
+
+    @WrapWithCondition(
+            method = "render(Lnet/minecraft/block/entity/SignBlockEntity;" +
+                    "Lnet/minecraft/client/util/math/MatrixStack;" +
+                    "Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/block/BlockState;" +
+                    "Lnet/minecraft/block/AbstractSignBlock;" +
+                    "Lnet/minecraft/block/WoodType;Lnet/minecraft/client/model/Model;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/block/entity/SignBlockEntityRenderer;" +
+                            "renderText(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/SignText;" +
+                            "Lnet/minecraft/client/util/math/MatrixStack;" +
+                            "Lnet/minecraft/client/render/VertexConsumerProvider;IIIZ)V",
+                    ordinal = 1
+            )
+    )
+    private boolean cullBackSignText(SignBlockEntityRenderer renderer, BlockPos pos, SignText text,
+                                     MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i,
+                                     int j, int i2, boolean l, @Local BlockState state, @Local Model model) {
+        return cullSignText(pos, state, model, false);
+    }
+
+    private boolean cullSignText(BlockPos pos, BlockState state, Model model, boolean front) {
         if (MoreCulling.CONFIG.signTextCulling) {
             Vec3d cameraPos = MinecraftClient.getInstance().gameRenderer.getCamera().getPos();
-            if (signModel.stick.visible) {
-                double angle = blockState.get(SignBlock.ROTATION) * ONE_SIGN_ROTATION;
-                if (MathUtils.isBehindLine(angle, signBlockEntity.getPos().toCenterPos(), cameraPos)) {
-                    matrixStack.pop();
-                    ci.cancel();
-                }
-            } else { // wall mounted, faster but looks trash xD
-                Direction dir = blockState.get(WallSignBlock.FACING);
-                if (shouldHideWallSignText(
+            if (model instanceof SignBlockEntityRenderer.SignModel signModel && !signModel.stick.visible) {
+                Direction dir = state.get(WallSignBlock.FACING);
+                return front == !shouldHideWallSignText(
                         dir,
-                        signBlockEntity
-                                .getPos()
-                                .toCenterPos()
-                                .subtract(dir.getOffsetX() * 0.39, 0, dir.getOffsetZ() * 0.39),
+                        pos.toCenterPos().subtract(dir.getOffsetX() * 0.39, 0, dir.getOffsetZ() * 0.39),
                         cameraPos
-                )) {
-                    matrixStack.pop();
-                    ci.cancel();
+                );
+            } else {
+                double angle = state.get(SignBlock.ROTATION) * ONE_SIGN_ROTATION;
+                if (front) { // Switch line orientation xD
+                    return !MathUtils.isBehindLine(angle, pos.toCenterPos(), cameraPos);
                 }
+                return !MathUtils.isBehindLine(angle, cameraPos, pos.toCenterPos());
             }
         }
+        return true;
     }
 }
