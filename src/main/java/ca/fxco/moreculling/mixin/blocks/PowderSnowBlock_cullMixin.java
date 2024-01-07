5334626678e81b9615d47c74c624c5c5ce32ac74
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-01-07 10:47:00 -0500

    Fix powdered snow culling - This does not make it possible to only cull a single side of the powdered snow, that's a lot more complicated to do. Thanks to Sodium and Indium

--- src/main/java/ca/fxco/moreculling/mixin/blocks/PowderSnowBlock_cullMixin.java
+++ src/main/java/ca/fxco/moreculling/mixin/blocks/PowderSnowBlock_cullMixin.java
@@ -2,7 +2,6 @@ package ca.fxco.moreculling.mixin.blocks;
 
 import ca.fxco.moreculling.MoreCulling;
 import ca.fxco.moreculling.api.block.MoreBlockCulling;
-import ca.fxco.moreculling.api.model.BakedOpacity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PowderSnowBlock;
@@ -15,10 +14,8 @@ import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.Optional;
 
-import static ca.fxco.moreculling.MoreCulling.blockRenderManager;
-
 @Mixin(value = PowderSnowBlock.class, priority = 1200)
-public class PowderSnowBlock_cullMixin extends Block implements MoreBlockCulling {
+public abstract class PowderSnowBlock_cullMixin extends Block implements MoreBlockCulling {
 
     public PowderSnowBlock_cullMixin(Settings settings) {
         super(settings);
@@ -26,23 +23,16 @@ public class PowderSnowBlock_cullMixin extends Block implements MoreBlockCulling
 
     @Override
     public boolean usesCustomShouldDrawFace(BlockState state) {
-        return MoreCulling.CONFIG.powderSnowCulling; //Normal powered snow culling will skip this check
+        return true; //Normal powered snow culling will skip this check
     }
 
     @Override
     public Optional<Boolean> customShouldDrawFace(BlockView view, BlockState thisState, BlockState sideState,
                                                   BlockPos thisPos, BlockPos sidePos, Direction side) {
-        return Optional.of(!(sideState.isOpaque() &&
-                sideState.isSideSolidFullSquare(view, sidePos, side.getOpposite())));
+        return Optional.of(!sideState.isOf(this));
     }
 
-    @Override
-    public boolean shouldAttemptToCull(BlockState state) {
-        return !((BakedOpacity) blockRenderManager.getModel(state)).hasTextureTranslucency(state);
-    }
-
-    @Override
     public VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
-        return !MoreCulling.CONFIG.powderSnowCulling ? VoxelShapes.fullCube() : VoxelShapes.empty();
+        return MoreCulling.CONFIG.powderSnowCulling ? VoxelShapes.fullCube() : VoxelShapes.empty();
     }
 }
