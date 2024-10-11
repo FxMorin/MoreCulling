e9b04edae987cfc4e6bda3bf53b1d4d2f573bb9d
Foxy <yarushas7@gmail.com>
2024-10-11 20:36:14 +0200

    Update to 1.21.2-pre3

--- common/src/main/java/ca/fxco/moreculling/mixin/LevelRenderer_cloudsMixin.java
+++ common/src/main/java/ca/fxco/moreculling/mixin/LevelRenderer_cloudsMixin.java
@@ -1,125 +1,141 @@
 package ca.fxco.moreculling.mixin;
 
-import ca.fxco.moreculling.MoreCulling;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
-import net.minecraft.client.CloudStatus;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.util.Mth;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.client.renderer.CloudRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Restriction(conflict = {
         @Condition("cullclouds"),
         @Condition("extended-clouds"),
         @Condition("extendedclouds")
 })
-@Mixin(LevelRenderer.class)
-public class LevelRenderer_cloudsMixin {
-
-    @Unique
-    private static final float SCALE = 0.00390625F;
+@Mixin(CloudRenderer.class)
+public abstract class LevelRenderer_cloudsMixin {
+    @Shadow
+    private static boolean isNorthEmpty(long l) {
+        return false;
+    }
 
     @Shadow
-    @Nullable
-    private CloudStatus prevCloudsType;
+    private static boolean isSouthEmpty(long l) {
+        return false;
+    }
+
+    @Shadow
+    private static boolean isWestEmpty(long l) {
+        return false;
+    }
+
+    @Shadow
+    private static boolean isEastEmpty(long l) {
+        return false;
+    }
 
     @Inject(
-            method = "buildClouds",
-            at = @At("HEAD"),
-            cancellable = true
+            method = "buildMesh(Lnet/minecraft/client/renderer/CloudRenderer$RelativeCameraPos;" +
+                    "Lcom/mojang/blaze3d/vertex/BufferBuilder;IIIIIIZ)V",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/renderer/CloudRenderer;" +
+                            "texture:Lnet/minecraft/client/renderer/CloudRenderer$TextureData;",
+                    ordinal = 3,
+                    shift = At.Shift.AFTER
+            )
     )
-    private void moreculling$renderClouds(Tesselator tesselator, double x, double y, double z, Vec3 color, CallbackInfoReturnable<MeshData> cir) {
-        if (!MoreCulling.CONFIG.cloudCulling) {
-            return;
+    private void moreculling$renderClouds(CloudRenderer.RelativeCameraPos relativeCameraPos,
+                                          BufferBuilder bufferBuilder, int i, int j, int k, int l, int m, int n,
+                                          boolean bl, CallbackInfo ci) {
+        RenderSystem.enableCull();
+    }
+
+    @Inject(
+            method = "buildExtrudedCell",
+            at = @At(value = "HEAD")
+    )
+    private void moreculling$renderClouds(CloudRenderer.RelativeCameraPos relativeCameraPos,
+                                          BufferBuilder bufferBuilder, int i, int j, int k,
+                                          int l, int m, int n, long o, CallbackInfo ci) {
+        float f = (float)m * 12.0F;
+        float g = f + 12.0F;
+        float h = 0.0F;
+        float p = 4.0F;
+        float q = (float)n * 12.0F;
+        float r = q + 12.0F;
+        if (relativeCameraPos != CloudRenderer.RelativeCameraPos.BELOW_CLOUDS) {
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(j);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(j);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(j);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(j);
         }
-        float k = (float) Mth.floor(x) * SCALE;
-        float l = (float) Mth.floor(z) * SCALE;
-        float m = (float) color.x;
-        float n = (float) color.y;
-        float o = (float) color.z;
-        float p = m * 0.9F;
-        float q = n * 0.9F;
-        float r = o * 0.9F;
-        float s = m * 0.7F;
-        float t = n * 0.7F;
-        float u = o * 0.7F;
-        float v = m * 0.8F;
-        float w = n * 0.8F;
-        float aa = o * 0.8F;
-        BufferBuilder builder = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR_NORMAL);
-        float ab = (float) Math.floor(y / 4.0) * 4.0F;
-        if (this.prevCloudsType == CloudStatus.FANCY) {
-            RenderSystem.enableCull(); // Enable culling
-            for (int ac = -3; ac <= 4; ++ac) {
-                for (int ad = -3; ad <= 4; ++ad) {
-                    float ae = (float) (ac * 8);
-                    float af = (float) (ad * 8);
-                    if (ab > -5.0F) { // bottom [-y] - Reversed Winding Order
-                        builder.addVertex(ae + 0.0F, ab + 0.0F, af + 0.0F).setUv((ae + 0.0F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(s, t, u, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                        builder.addVertex(ae + 8.0F, ab + 0.0F, af + 0.0F).setUv((ae + 8.0F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(s, t, u, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                        builder.addVertex(ae + 8.0F, ab + 0.0F, af + 8.0F).setUv((ae + 8.0F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(s, t, u, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                        builder.addVertex(ae + 0.0F, ab + 0.0F, af + 8.0F).setUv((ae + 0.0F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(s, t, u, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                    }
-                    if (ab <= 5.0F) { // top [+y]
-                        builder.addVertex(ae + 0.0F, ab + 4.0F - 9.765625E-4F, af + 8.0F).setUv((ae + 0.0F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, 1.0F, 0.0F);
-                        builder.addVertex(ae + 8.0F, ab + 4.0F - 9.765625E-4F, af + 8.0F).setUv((ae + 8.0F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, 1.0F, 0.0F);
-                        builder.addVertex(ae + 8.0F, ab + 4.0F - 9.765625E-4F, af + 0.0F).setUv((ae + 8.0F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, 1.0F, 0.0F);
-                        builder.addVertex(ae + 0.0F, ab + 4.0F - 9.765625E-4F, af + 0.0F).setUv((ae + 0.0F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, 1.0F, 0.0F);
-                    }
-                    int ag;
-                    if (ac > -1) { // west [-x]
-                        for (ag = 0; ag < 8; ++ag) {
-                            builder.addVertex(ae + (float) ag + 0.0F, ab + 0.0F, af + 8.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(-1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 0.0F, ab + 4.0F, af + 8.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(-1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 0.0F, ab + 4.0F, af + 0.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(-1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 0.0F, ab + 0.0F, af + 0.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(-1.0F, 0.0F, 0.0F);
-                        }
-                    }
-                    if (ac <= 1) { // east [+x] - Reversed Winding Order
-                        for (ag = 0; ag < 8; ++ag) {
-                            builder.addVertex(ae + (float) ag + 1.0F - 9.765625E-4F, ab + 0.0F, af + 0.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 1.0F - 9.765625E-4F, ab + 4.0F, af + 0.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 0.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 1.0F - 9.765625E-4F, ab + 4.0F, af + 8.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(1.0F, 0.0F, 0.0F);
-                            builder.addVertex(ae + (float) ag + 1.0F - 9.765625E-4F, ab + 0.0F, af + 8.0F).setUv((ae + (float) ag + 0.5F) * SCALE + k, (af + 8.0F) * SCALE + l).setColor(p, q, r, 0.8F).setNormal(1.0F, 0.0F, 0.0F);
-                        }
-                    }
-                    if (ad > -1) { // north [-z]
-                        for (ag = 0; ag < 8; ++ag) {
-                            builder.addVertex(ae + 0.0F, ab + 4.0F, af + (float) ag + 0.0F).setUv((ae + 0.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, -1.0F);
-                            builder.addVertex(ae + 8.0F, ab + 4.0F, af + (float) ag + 0.0F).setUv((ae + 8.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, -1.0F);
-                            builder.addVertex(ae + 8.0F, ab + 0.0F, af + (float) ag + 0.0F).setUv((ae + 8.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, -1.0F);
-                            builder.addVertex(ae + 0.0F, ab + 0.0F, af + (float) ag + 0.0F).setUv((ae + 0.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, -1.0F);
-                        }
-                    }
-                    if (ad <= 1) { // south [+z] - Reversed Winding Order
-                        for (ag = 0; ag < 8; ++ag) {
-                            builder.addVertex(ae + 0.0F, ab + 0.0F, af + (float) ag + 1.0F - 9.765625E-4F).setUv((ae + 0.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, 1.0F);
-                            builder.addVertex(ae + 8.0F, ab + 0.0F, af + (float) ag + 1.0F - 9.765625E-4F).setUv((ae + 8.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, 1.0F);
-                            builder.addVertex(ae + 8.0F, ab + 4.0F, af + (float) ag + 1.0F - 9.765625E-4F).setUv((ae + 8.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, 1.0F);
-                            builder.addVertex(ae + 0.0F, ab + 4.0F, af + (float) ag + 1.0F - 9.765625E-4F).setUv((ae + 0.0F) * SCALE + k, (af + (float) ag + 0.5F) * SCALE + l).setColor(v, w, aa, 0.8F).setNormal(0.0F, 0.0F, 1.0F);
-                        }
-                    }
-                }
-            }
-        } else {
-            for (int ah = -32; ah < 32; ah += 32) {
-                for (int ai = -32; ai < 32; ai += 32) {
-                    builder.addVertex(ah, ab, ai + 32).setUv((float) (ah) * SCALE + k, (float) (ai + 32) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                    builder.addVertex(ah + 32, ab, ai + 32).setUv((float) (ah + 32) * SCALE + k, (float) (ai + 32) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                    builder.addVertex(ah + 32, ab, ai).setUv((float) (ah + 32) * SCALE + k, (float) (ai) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                    builder.addVertex(ah, ab, ai).setUv((float) (ah) * SCALE + k, (float) (ai) * SCALE + l).setColor(m, n, o, 0.8F).setNormal(0.0F, -1.0F, 0.0F);
-                }
-            }
+
+        if (relativeCameraPos != CloudRenderer.RelativeCameraPos.ABOVE_CLOUDS) {
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(i);
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(i);
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(i);
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(i);
         }
-        cir.setReturnValue(builder.buildOrThrow());
+
+        if (isNorthEmpty(o) && n > 0) {
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(l);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(l);
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(l);
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(l);
+        }
+
+        if (isSouthEmpty(o) && n < 0) {
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(l);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(l);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(l);
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(l);
+        }
+
+        if (isWestEmpty(o) && m > 0) {
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(k);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(k);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(k);
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(k);
+        }
+
+        if (isEastEmpty(o) && m < 0) {
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(k);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(k);
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(k);
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(k);
+        }
+
+        boolean bl = Math.abs(m) <= 1 && Math.abs(n) <= 1;
+        if (bl) {
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(j);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(j);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(j);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(j);
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(i);
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(i);
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(i);
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(i);
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(l);
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(l);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(l);
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(l);
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(l);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(l);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(l);
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(l);
+            bufferBuilder.addVertex(f, 0.0F, q).setColor(k);
+            bufferBuilder.addVertex(f, 4.0F, q).setColor(k);
+            bufferBuilder.addVertex(f, 4.0F, r).setColor(k);
+            bufferBuilder.addVertex(f, 0.0F, r).setColor(k);
+            bufferBuilder.addVertex(g, 0.0F, r).setColor(k);
+            bufferBuilder.addVertex(g, 4.0F, r).setColor(k);
+            bufferBuilder.addVertex(g, 4.0F, q).setColor(k);
+            bufferBuilder.addVertex(g, 0.0F, q).setColor(k);
+        }
     }
 }
--- common/src/main/java/ca/fxco/moreculling/mixin/LevelRenderer_rainMixin.java
+++ common/src/main/java/ca/fxco/moreculling/mixin/LevelRenderer_rainMixin.java
@@ -1,35 +1,26 @@
 package ca.fxco.moreculling.mixin;
 
 import ca.fxco.moreculling.MoreCulling;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.Tesselator;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.WeatherEffectRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(LevelRenderer.class)
+@Mixin(WeatherEffectRenderer.class)
 public class LevelRenderer_rainMixin {
-
-    @Shadow
-    private Frustum cullingFrustum;
-
     @Inject(
-            method = "renderSnowAndRain",
+            method = "getPrecipitationAt",
             locals = LocalCapture.CAPTURE_FAILSOFT,
             at = @At(
                     value = "INVOKE",
@@ -38,16 +29,11 @@ public class LevelRenderer_rainMixin {
                     shift = At.Shift.BEFORE
             )
     )
-    private void moreculling$checkRainFrustum(LightTexture manager, float tickDelta, double cameraX,
-                                              double cameraY, double cameraZ, CallbackInfo ci, float f, Level level,
-                                              int i, int j, int k, Tesselator tessellator,
-                                              BufferBuilder bufferBuilder, int l, int m, float g,
-                                              BlockPos.MutableBlockPos mutable,
-                                              @Share("skipLoop") LocalBooleanRef skipLoopRef) {
+    private void moreculling$checkRainFrustum(Level level, BlockPos mutable, CallbackInfoReturnable<Biome.Precipitation> cir, @Share("skipLoop") LocalBooleanRef skipLoopRef) {
         if (!MoreCulling.CONFIG.rainCulling) {
             return;
         }
-        skipLoopRef.set(!this.cullingFrustum.isVisible(new AABB(
+        skipLoopRef.set(!Minecraft.getInstance().levelRenderer.cullingFrustum.isVisible(new AABB(
                 mutable.getX() + 1,
                 level.getHeight(),
                 mutable.getZ() + 1,
@@ -57,15 +43,18 @@ public class LevelRenderer_rainMixin {
         )));
     }
 
-    @WrapOperation(
-            method = "renderSnowAndRain",
+    @Inject(
+            method = "getPrecipitationAt",
             at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/biome/Biome;hasPrecipitation()Z"
+                    value = "RETURN",
+                    ordinal = 1
             )
     )
-    private boolean moreculling$skipRainLoop(Biome instance, Operation<Boolean> original,
-                                             @Share("skipLoop") LocalBooleanRef skipLoopRef) {
-        return !skipLoopRef.get() && original.call(instance);
+    private void moreculling$skipRainLoop(Level level, BlockPos blockPos,
+                                          CallbackInfoReturnable<Biome.Precipitation> cir,
+                                          @Share("skipLoop") LocalBooleanRef skipLoopRef, @Local Biome biome) {
+        if (skipLoopRef.get() && !biome.hasPrecipitation()) {
+            cir.setReturnValue(Biome.Precipitation.NONE);
+        }
     }
 }
