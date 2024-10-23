b927bb17e5b7469c8187dad75100d48b3c188550
Foxy <yarushas7@gmail.com>
2024-10-23 17:15:27 +0200

    Prevent mod from loading on the server on neoforge

--- neoforge/src/main/java/ca/fxco/moreculling/MoreCullingNeoforge.java
+++ neoforge/src/main/java/ca/fxco/moreculling/MoreCullingNeoforge.java
@@ -1,11 +1,12 @@
 package ca.fxco.moreculling;
 
 import ca.fxco.moreculling.config.ModMenuConfig;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(MoreCulling.MOD_ID)
+@Mod(value = MoreCulling.MOD_ID, dist = Dist.CLIENT)
 public class MoreCullingNeoforge {
     public MoreCullingNeoforge(ModContainer container) {
         MoreCulling.init();
