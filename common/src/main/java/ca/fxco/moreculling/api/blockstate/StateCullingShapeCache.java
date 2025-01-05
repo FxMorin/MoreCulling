eb37902a03f67b30144506a0b8deae582406e5ed
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-01-05 16:48:25 -0500

    Small cleanup

--- common/src/main/java/ca/fxco/moreculling/api/blockstate/StateCullingShapeCache.java
+++ common/src/main/java/ca/fxco/moreculling/api/blockstate/StateCullingShapeCache.java
@@ -3,8 +3,20 @@ package ca.fxco.moreculling.api.blockstate;
 import net.minecraft.core.Direction;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+/**
+ * StateCullingShapeCache is an interface to access the shape cache.
+ *
+ * @since 1.2.3
+ */
 public interface StateCullingShapeCache {
 
+    /**
+     * Get the voxelshape of a culling face
+     *
+     * @param face The face to get the shape from
+     * @return The VoxelShape for the face.
+     * @since 1.2.3
+     */
     VoxelShape moreculling$getFaceCullingShape(Direction face);
 
 }
