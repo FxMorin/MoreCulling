85e75559e8ebf2b645cc1502b571e090fec508a8
1foxy2 <yarushas7@gmail.com>
2025-06-20 23:08:14 +0200

    Port to 1.21.6

--- common/src/main/java/ca/fxco/moreculling/mixin/CloudRenderer_cloudsMixin.java
+++ common/src/main/java/ca/fxco/moreculling/mixin/CloudRenderer_cloudsMixin.java
@@ -1,19 +1,21 @@
 package ca.fxco.moreculling.mixin;
 
-import ca.fxco.moreculling.MoreCulling;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.minecraft.client.CloudStatus;
 import net.minecraft.client.renderer.CloudRenderer;
-import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Restriction(conflict = {
+/*@Restriction(conflict = { TODO adapt this to 1.21.6
         @Condition("cullclouds"),
         @Condition("extended-clouds"),
         @Condition("extendedclouds")
@@ -135,4 +137,4 @@ public abstract class CloudRenderer_cloudsMixin {
         }
         ci.cancel();
     }
-}
+}*/
