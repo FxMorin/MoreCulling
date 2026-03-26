00419dc2591c7f54bc3d9929a17228a5c3d2df4f
1foxy2 <yarushas7@gmail.com>
2026-03-26 19:50:14 +0100

    Fix sodium compat

--- common/src/main/java/ca/fxco/moreculling/mixin/compat/BlockOcclusionCache_sodiumMixin.java
+++ common/src/main/java/ca/fxco/moreculling/mixin/compat/BlockOcclusionCache_sodiumMixin.java
@@ -5,12 +5,15 @@ import ca.fxco.moreculling.utils.CullingUtils;
 import com.llamalad7.mixinextras.sugar.Local;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.caffeinemc.mods.sodium.client.render.model.AbstractBlockRenderContext;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,24 +24,32 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
  */
 @Restriction(require = @Condition("sodium"))
 @Pseudo
-@Mixin(targets = "net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache")
+@Mixin(AbstractBlockRenderContext.class)
 public class BlockOcclusionCache_sodiumMixin {
+
+    @Shadow
+    protected BlockState state;
+
+    @Shadow
+    protected BlockAndTintGetter level;
+
+    @Shadow
+    protected BlockPos pos;
 
     @Inject(
             method = "shouldDrawSide",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/BlockGetter;getBlockState(" +
-                            "Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;",
+                    target = "Lnet/minecraft/client/renderer/block/BlockAndTintGetter;" +
+                            "getBlockState(Lnet/minecraft/core/BlockPos;" +
+                            ")Lnet/minecraft/world/level/block/state/BlockState;",
                     shift = At.Shift.BEFORE
             ),
             cancellable = true
     )
-    private void moreculling$useMoreCulling(BlockState selfState, BlockGetter view, BlockPos pos,
-                                            Direction facing, CallbackInfoReturnable<Boolean> cir,
-                                            @Local BlockPos.MutableBlockPos adjPos) {
+    private void moreculling$useMoreCulling(Direction facing, CallbackInfoReturnable<Boolean> cir, @Local BlockPos.MutableBlockPos adjPos) {
         if (MoreCulling.CONFIG.useBlockStateCulling) {
-            cir.setReturnValue(CullingUtils.shouldDrawSideCulling(selfState, view.getBlockState(adjPos), view, pos, facing, adjPos));
+            cir.setReturnValue(CullingUtils.shouldDrawSideCulling(state, level.getBlockState(adjPos), level, pos, facing, adjPos));
         }
     }
 }
